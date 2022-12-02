639a4b24392eaeaf6d0c10a2d7073901b01ea623
C-atfish <jonlar@student.kristiania.no>
2022-12-02 03:13:36 +0100

    Added the newly created bucket to the backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,5 +5,9 @@ terraform {
       version = "4.40.0"
     }
   }
-
+  backend "s3" {
+    bucket = "analytics-1023"
+    key    = "/state/${var.candidate_id}terraform.state"
+    region = "eu-north-1"
+  }
 }
