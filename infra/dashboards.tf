7cd856b34b548d20499318603d1a6a181500a262
C-atfish <jonlar@student.kristiania.no>
2022-12-02 17:35:21 +0100

    Added singleValue view to

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -40,7 +40,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "stat": "Maximum",
         "region": "eu-west-1",
         "title": "Total number of current carts",
-        "view": "singleValue"
+        "view": "singleValue",
         "legend": {"position": "right"}
       }
     },
