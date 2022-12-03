8a03ff910214ed1182d9c1a19cfe352995be8fa5
C-atfish <jonlar@student.kristiania.no>
2022-12-03 15:10:10 +0100

    Added alarm and my student mail to SNS

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,3 +1,21 @@
+resource "aws_cloudwatch_metric_alarm" "zerosum" {
+  alarm_name                = "More than 5 Carts for 15 min - 1023"
+  namespace                 = "1023"
+  metric_name               = "number_of_active_carts.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "5"
+  evaluation_periods        = "3"
+  period                    = "300"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "This alarm goes off as soon as the total amount of money in the bank exceeds 0 "
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+}
+
+
 resource "aws_sns_topic" "alarms" {
   name = "alarm-topic-${var.candidate_id}"
 }
